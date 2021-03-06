5519f9b060698dd9920e97efbdb883d97ad41f02
Ashim Raj Konwar <ashimrajkonwar@gmail.com>
2021-03-06 13:50:46 +0530

    Add custom accordion component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { Navigation } from './Navigation/Navigation';
 export { SideDrawer } from './SideDrawer/SideDrawer';
 export { Sidebar } from './Sidebar/Sidebar';
 export { Badge } from './Badge/Badge';
+export { CustomAccordion as Accordion } from './CustomAccordion/CustomAccordion';
